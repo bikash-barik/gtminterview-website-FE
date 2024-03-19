904e8513474e88f2e13d8a44e9fd84fc7a33656b
sauravsable <sauravsable4102@gmail.com>
2024-03-19 17:25:01 +0530

    all service added

--- src/components/ServiceDescription/ServiceDescription.jsx
+++ src/components/ServiceDescription/ServiceDescription.jsx
@@ -12,7 +12,7 @@ const data = {
 
 }
 export default function ServiceDescription({item}) {
-  console.log("data in service description",item);
+  console.log("data in service description",item.techUsed);
   return (
     <section className="servicedescriptionsection">
 
@@ -20,7 +20,24 @@ export default function ServiceDescription({item}) {
         <div className="servicedescriptioncolumn1">
           <p className="servicedescriptionpara"  data-aos="fade-up">{item.description1}</p>
           {/* <img className="servicedescriptionimg" src={serviceimg} alt="" /> */}
-          <ServicesName item={item.techUsed}/>
+          {/* <ServicesName item={item}/> */}
+          {/* <section className='servicesnamesection'>
+        <div className="servicesnamerow"> 
+            {
+                item.techUsed.map((item,index)=>(
+                    <div className="servicesnamecolumn" key={index}>
+                    <div className="servicesnameimgcolumn">
+                        <img className='servicenameimg' src={item.imageUrl} alt="" />
+                    </div>
+    
+                    <div className="servicesnameheadingcolumn">
+                        <p className='servicenameheading'>{item.textInput}</p>
+                    </div>
+                </div>
+                ))
+            }
+        </div>
+    </section> */}
           <p className="servicedescriptionpara" data-aos="fade-up">{item.description2}</p>
           <img className="servicedescriptionimg" src={item.serviceimage} alt="developmentimage" data-aos="fade-up"/>
         </div>
--- src/components/ServicesName/ServicesName.jsx
+++ src/components/ServicesName/ServicesName.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './servicesname.css';
 import googleplus from '../../assets/images/googleplus.png';
 import facebook from '../../assets/images/Facebook-logo-2048x1280.png';
@@ -17,29 +17,22 @@ const data =[
         serviceimage : linkedin,
         servicename : "LinkedIn"
     },
-    {
-        serviceimage : googleplus,
-        servicename : "Google Plus"
-    },
-    {
-        serviceimage : facebook,
-        servicename : "Facebook"
-    },
-    {
-        serviceimage : linkedin,
-        servicename : "LinkedIn"
-    },
-
 ]
 
 export default function ServicesName({item}) {
     console.log("item inside services name",item);
+
+    const [techusedData, settechusedData] = useState(data);
+
+    useEffect(()=>{
+        settechusedData(item.techusedData);
+    },[settechusedData,item.techusedData])
   return (
     <section className='servicesnamesection'>
         <div className="servicesnamerow"> 
             {
-                item.map((item,index)=>(
-                    <div className="servicesnamecolumn">
+                techusedData.map((item,index)=>(
+                    <div className="servicesnamecolumn" key={index}>
                     <div className="servicesnameimgcolumn">
                         <img className='servicenameimg' src={item.imageUrl} alt="" />
                     </div>
